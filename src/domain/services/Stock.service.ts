4e79c1dc58af20fb3a35bf37c4482939bfd9c29c
SuhaibGhattass21 <ghattasssuhaib892004@gmail.com>
2025-07-16 13:58:41 +0300

    feat: remove unused services

--- src/domain/services/Stock.service.ts
+++
@@ -1,9 +0,0 @@
-import { InventoryRepository } from '../../infrastructure/repositories/inventory.repositry.impl';
-
-export class InventoryService {
-    private inventoryRepo = new InventoryRepository();
-
-    async getUsageByShift(shiftId: string) {
-        return this.inventoryRepo.getInventoryUsageByShift(shiftId);
-    }
-}
