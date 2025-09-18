78f3de6cc7b9b7a459293c83911d8174e7eb6c93
SuhaibGhattass21 <ghattasssuhaib892004@gmail.com>
2025-09-18 04:46:11 +0300

    fix: remove optional chaining for order_item_id in mapExtraToResponseDto

--- src/application/use-cases/Orders/order.use-cases.ts
+++ src/application/use-cases/Orders/order.use-cases.ts
@@ -347,7 +347,6 @@ export class OrderUseCases {
   private mapExtraToResponseDto(extra: OrderItemExtra): any {
     return {
       order_item_extra_id: extra.order_item_extra_id,
-      order_item_id: extra.orderItem.order_item_id || "",
       extra: extra.extra
         ? {
           extra_id: extra.extra.extra_id,
